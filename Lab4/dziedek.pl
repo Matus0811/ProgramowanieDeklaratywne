b8d47cef27b6ca9dcb0a26fc0f07b0f03830b8c1
Matus0811 <kamil.matusiak2001@wp.pl>
2023-03-13 14:20:04 +0100

    PASTE

---
+++ Lab4/dziedek.pl
@@ -0,0 +1,8 @@
+ojciec(karol,jann).
+ojciec(karol,adam).
+
+dziecko(tomasz,jann).
+dziecko(piotr,adam).
+
+dziadek(karol,marek).
+dziadek(X,Y):-ojciec(X,Z),dziecko(Y,Z).
---
+++ Lab4/zeleżności.pl~
@@ -0,0 +1,8 @@
+p(a,d).
+p(X,Y):-q(X,Z), r(Z,Y).
+
+q(a,b).
+q(c,a).
+
+r(a,d).
+r(b,c).
